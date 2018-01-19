9c0a5592433337938de40260d5fb1f6a7f4173e6
davemfish <davemfish@gmail.com>
2018-01-19 10:01:44 -0800

    buffer(0) before unary_union

--- pca_unaryunion_slrscenarios.py
+++ pca_unaryunion_slrscenarios.py
@@ -1,3 +1,5 @@
+# source activate geowork
+
 import geopandas as gpd
 import os
 import glob
@@ -5,19 +7,20 @@ import glob
 basedir = '../data/pca/bcdc_slr/PCAexposure_201710/slr_scenarios/'
 for scenario in os.listdir(basedir):
 	print(scenario)
-
-	pcafile = glob.glob(os.path.join(basedir, scenario, 'dis*.shp'))[0]
-	shp = gpd.read_file(pcafile)
-	print('dissolving', pcafile)
-
-	geom = shp['geometry']
-	unioned = gpd.GeoSeries(geom.unary_union)
-	outdf = shp.iloc[0:1]
-	geo_df = gpd.GeoDataFrame(outdf, crs=shp.crs, geometry=gpd.GeoSeries(unioned))
-
 	outdir = os.path.join(basedir, scenario, 'unaryunion')
 	if not os.path.exists(outdir):
-	    os.makedirs(outdir)
 
-	print(outdir)
-	geo_df.to_file(os.path.join(outdir, 'diss.shp'))
+		pcafile = glob.glob(os.path.join(basedir, scenario, 'dis*.shp'))[0]
+		shp = gpd.read_file(pcafile)
+		print('buffering', pcafile)
+		
+		print('dissolving', pcafile)
+		geom = shp['geometry']
+		geom = geom.buffer(0)
+		unioned = gpd.GeoSeries(geom.unary_union)
+		outdf = shp.iloc[0:1]
+		geo_df = gpd.GeoDataFrame(outdf, crs=shp.crs, geometry=gpd.GeoSeries(unioned))
+
+		print(outdir)
+		os.makedirs(outdir)
+		geo_df.to_file(os.path.join(outdir, 'diss.shp'))

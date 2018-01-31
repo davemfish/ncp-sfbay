7531ec1b81a1e97313f8b3604c11c23fb6adaa55
davemfish <davemfish@gmail.com>
2018-01-31 10:18:50 -0800

    unary union for all the PCA x SLR intersections,

--- pca_unaryunion_slrscenarios.py
+++ pca_unaryunion_slrscenarios.py
@@ -1,3 +1,9 @@
+## unary union for all the PCA x SLR intersections,
+## these unioned PCA networks will support 'system-level'
+## PUD counting for each scenario.
+
+## after this step, need to remove developed lulc types from these networks
+
 # source activate geowork
 
 import geopandas as gpd

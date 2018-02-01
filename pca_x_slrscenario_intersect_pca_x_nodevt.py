6180b5f6536ad318217af37ce8da775d1d18d5b0
davemfish <dfisher5@vulpes.sefs.uw.edu>
2018-02-01 11:35:46 -0800

    fix some small errors, syntax, etc. Also wrap in function that takes a slrscenario name, which will make it easy to spread this work across multiple cores.

--- pca_x_slrscenario_intersect_pca_x_nodevt.py
+++ pca_x_slrscenario_intersect_pca_x_nodevt.py
@@ -17,53 +17,62 @@ pca_nodev_path = '../data/bcdc_othernaturalareas/System_level_AOIs_Recmodeling/P
 nodevshp = gpd.read_file(pca_nodev_path)
 nodevshp['pca_key'] = nodevshp['fipco'] + nodevshp['joinkey']
 
+BASEDIR = '../PCAexposure_201710/slr_scenarios/'
 
-basedir = '../PCAexposure_201710/slr_scenarios/'
-for scenario in os.listdir(basedir):
+def intersect_byeach_pca(scenario):
+
     print(scenario)
-    scenariodir = os.path.join(basedir, scenario)
-    outdir = os.path.join(scenariodir 'nodevt')
+    scenariodir = os.path.join(BASEDIR, scenario)
+    outdir = os.path.join(scenariodir, 'nodevt')
     if not os.path.exists(outdir):
         # the pca network intersection with the SLR scenario
         pcafile = glob.glob(os.path.join(scenariodir, 'dis*.shp'))[0]
         pcashp = gpd.read_file(pcafile)
 
-        pcashp.crs == nodevshp.crs
-        proj4string = '+proj=utm +zone=10 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
+    #     pcashp.crs == nodevshp.crs
+    #     proj4string = '+proj=utm +zone=10 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
 
-        # table linking scenario polygon id (TARGET_FID) and master PCA unique id (joinkey, fipco)
-        # TARGET_FID == FID_spjoin
-        fidspjoin_joinkey_crswlk = gpd.read_file(glob.glob(os.path.join(scenariodir, 'spjoi*.dbf'))[0])
-        fidspjoin_joinkey_crswlk = fidspjoin_joinkey_crswlk[['TARGET_FID', 'joinkey', 'fipco', 'name']]
-        fidspjoin_joinkey_crswlk['pca_key'] = fidspjoin_joinkey_crswlk['fipco'] + fidspjoin_joinkey_crswlk['joinkey']
+    #     # table linking scenario polygon id (TARGET_FID) and master PCA unique id (joinkey, fipco)
+    #     # TARGET_FID == FID_spjoin
+    #     fidspjoin_joinkey_crswlk = gpd.read_file(glob.glob(os.path.join(scenariodir, 'spjoi*.dbf'))[0])
+    #     fidspjoin_joinkey_crswlk = fidspjoin_joinkey_crswlk[['TARGET_FID', 'joinkey', 'fipco', 'name']]
+    #     fidspjoin_joinkey_crswlk['pca_key'] = fidspjoin_joinkey_crswlk['fipco'] + fidspjoin_joinkey_crswlk['joinkey']
 
-        ## for each pca in pcashp, find the corresponding geom from nodevshp, intersect, save geometry
-        collection = []
-        fids = []
-        for index, row in pcashp.iterrows():
-            fid = row['FID_spjoin']
-            fids.append(fid)
-            intersector_key = fidspjoin_joinkey_crswlk[fidspjoin_joinkey_crswlk['TARGET_FID'] == fid]['pca_key']
-            if len(intersector_key) == 1:
-                intersector_key = intersector_key.values[0]
-                print(intersector_key)
-            else:
-                print('more than one pca matched')
-            intersector_geoseries = nodevshp[nodevshp['pca_key'] == intersector_key]['geometry']
-            intersector_geom = intersector_geoseries.unary_union
-            print(datetime.datetime.now())
-            geom1 = row['geometry'].buffer(0)
-            geom2 = intersector_geom.buffer(0)
-            try:
-                result_geom = geom1.intersection(geom2)
-            except TopologicalError:
-                print('found topology error, trying buffer of 1 millimeter')
-                result_geom = geom1.intersection(geom2.buffer(0.001))
-            collection.append(result_geom)
+    #     ## for each pca in pcashp, find the corresponding geom from nodevshp, intersect, save geometry
+    #     collection = []
+    #     fids = []
+    #     for index, row in pcashp.iterrows():
+    #         fid = row['FID_spjoin']
+    #         fids.append(fid)
+    #         intersector_key = fidspjoin_joinkey_crswlk[fidspjoin_joinkey_crswlk['TARGET_FID'] == fid]['pca_key']
+    #         if len(intersector_key) == 1:
+    #             intersector_key = intersector_key.values[0]
+    #             print(intersector_key)
+    #         else:
+    #             print('more than one pca matched')
+    #         intersector_geoseries = nodevshp[nodevshp['pca_key'] == intersector_key]['geometry']
+    #         intersector_geom = intersector_geoseries.unary_union
+    #         print(datetime.datetime.now())
+    #         geom1 = row['geometry'].buffer(0)
+    #         geom2 = intersector_geom.buffer(0)
+    #         try:
+    #             result_geom = geom1.intersection(geom2)
+    #         except TopologicalError:
+    #             print('found topology error, trying buffer of 1 millimeter')
+    #             result_geom = geom1.intersection(geom2.buffer(0.001))
+    #         collection.append(result_geom)
 
+        os.makedirs(outdir)
+    #     outgeo = gpd.GeoSeries(collection)
+    #     outdf = gpd.GeoDataFrame(data={'FID_spjoin':fids}, geometry=outgeo, crs=proj4string)
+    #     outdf = outdf[~outdf['geometry'].is_empty]
 
-        outgeo = gpd.GeoSeries(collection)
-        outdf = gpd.GeoDataFrame(data={'FID_spjoin':fids}, geometry=outgeo, crs=proj4string)
-        outdf = outdf[~outdf['geometry'].is_empty]
+        outdf = pcashp
         outdf.to_file(os.path.join(outdir, 'diss_nodevt.shp'))
 
+
+
+
+scenario_list = ['12inches']
+for s in scenario_list:
+    intersect_byeach_pca(s)
